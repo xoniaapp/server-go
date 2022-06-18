056bc9303155c1ce0e2688adf84d21afc6e732ac
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2022-06-18 21:03:53 +0600

    minify

--- model/interfaces.go
+++ model/interfaces.go
@@ -5,22 +5,16 @@ import (
 	"mime/multipart"
 )
 
-// FileRepository defines methods related to file upload the service layer expects
-// any repository it interacts with to implement
 type FileRepository interface {
 	UploadAvatar(header *multipart.FileHeader, directory string) (string, error)
 	UploadFile(header *multipart.FileHeader, directory, filename, mimetype string) (string, error)
 	DeleteImage(key string) error
 }
 
-// MailRepository defines methods related to mail operations the service layer expects
-// any repository it interacts with to implement
 type MailRepository interface {
 	SendResetMail(email string, html string) error
 }
 
-// RedisRepository defines methods related to the redis db the service layer expects
-// any repository it interacts with to implement
 type RedisRepository interface {
 	SetResetToken(ctx context.Context, id string) (string, error)
 	GetIdFromToken(ctx context.Context, token string) (string, error)
